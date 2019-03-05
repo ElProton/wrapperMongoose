29c57954bfcbe5fa33431a27d9be5d1512a3f25a
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2019-03-05 06:23:02 +0100

    ajuste les tailles des malloc()

--- Mongoose-edgesep/mongooseApplication/connectorTest.c
+++ Mongoose-edgesep/mongooseApplication/connectorTest.c
@@ -11,9 +11,6 @@
 
 int main(int argc, char **argv)
 {
-	int ch,i,px,val,n;
-	int64_t val64;
-	double vald;
 	struct option longopts[6] = {
 		{"A", no_argument, NULL, 'A'},
 		{"B", no_argument, NULL, 'B'},
@@ -39,40 +36,38 @@ int main(int argc, char **argv)
 
 	spasm_triplet *T = spasm_load_mm(stdin, -1);
 	assert(T->n == T->m);
+	
+	/* élimine les entrées sur la diagonale */
 	int *Ti = T->i;
 	int *Tj = T->j;
-	for (px = 0; px < T->nz; px++) {
+	for (int px = 0; px < T->nz; px++)
 		if (Ti[px] == Tj[px]) {
 			spasm_swap(Ti, px, T->nz - 1);
 			spasm_swap(Tj, px, T->nz - 1);
 			T->nz--;
 		}
-	}
 
 	spasm *A = spasm_compress(T);
 	spasm_triplet_free(T);
 
-	if(mode=='B'){
-	struct modular_partition_t *partition = modular_partition(A);
-	A = partition -> M;
+	if (mode=='B') {
+		struct modular_partition_t *partition = modular_partition(A);
+		A = partition -> M;
 	}
 
-	n = A -> n;
-	int64_t *ap64 = spasm_calloc(n, sizeof(int64_t));
-	for(i = 0; i <= n; i++){
-		val = A->p[i];
-		val64 = (int64_t) val;
-		ap64[i] = val64;
-	}
+	int n = A -> n;
+	int *Ap = A->p;
+	int *Aj = A->j;
 
-	n = A -> m;
-	int64_t *aj64 = spasm_calloc(n, sizeof(int64_t));
-	for(i = 0; i <= n; i++){
-		val = A->j[i];
-		val64 = (int64_t) val;
-		aj64[i] = val64;
-	}
+	int64_t *ap64 = spasm_calloc(n + 1, sizeof(int64_t));
+	int64_t *aj64 = spasm_calloc(spasm_nnz(A), sizeof(int64_t));
 
+	for (int i = 0; i < n; i++)
+		ap64[i] = Ap[i];
+
+	for (int i = 0; i < spasm_nnz(A); i++)
+		aj64[i] = Aj[i];
+	
 	/*n = A -> m;
 	double *axd = spasm_calloc(n, sizeof(double));
 	for(i = 0; i <= n; i++){
@@ -83,21 +78,20 @@ int main(int argc, char **argv)
 
 	GraphC *g =  spasm_malloc(sizeof(*g));
 		
-
-	g->n = A->n;
-	g->m = A->m;
-	g->nz = A->p[A->n];
-	g->p = ap64;    /* problème de type */
-	g->i = aj64;    /* problème de type */
-    	g->x = NULL;     /* problème de type */
+	g->n = n;
+	g->m = n;
+	g->nz = spasm_nnz(A);
+	g->p = ap64;
+	g->i = aj64;
+    	g->x = NULL;
 	g->w = NULL;
 
-	//spasm_csr_free(A);
 	connector_edge_cut(g);
 
 	free(g);
 	free(aj64);
 	free(ap64);
+	spasm_csr_free(A);
 
 	return 0;
 }
